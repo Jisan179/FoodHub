f31a7ae67814834aa125bc05387d1caa14f3a58b
Jisan <130171392+Jisan179@users.noreply.github.com>
2026-09-02 03:15:04 +0600

    refactor: remove braces from redundant conditional redirect in index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 require_once __DIR__ . '/includes/auth_check.php';
 
-if (is_logged_in()) {
+if (is_logged_in()) 
     header("Location: " . get_user_dashboard_url());
 
  * FoodHub - Root Entrypoint Redirect
